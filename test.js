f01e5ad58d5434ac44ce0e4e20de8c0b5a74f875
Jay Trivedi <trivedija@pmc.local>
2024-03-14 13:32:39 +0530

    First COmmit

---
+++ test.js
@@ -0,0 +1,18 @@
+var number1 = 5;
+var number2 = 10;
+sum(number1,number2);
+subtract (number1,number2);
+
+function sum( number1, number2)
+
+{
+  var addition = number1+number2; 
+  console.log("addition of number " + addition);
+}
+
+function subtract( number1, number2)
+
+{
+  var subtraction = number1-number2; 
+  console.log("subtraction of number " + subtraction);
+}
